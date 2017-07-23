fdc3469aa47026a609bca3f0a7c18ec0c0ce8b95
Prajval <prajval0016@gmail.com>
2017-07-23 12:55:25 +0530

    Add a helper to return full title for page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def return_full_title_for_page(page_title = '')
+    base_title = "My Twitter App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
